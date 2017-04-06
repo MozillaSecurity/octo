ed2884cc3825b7b54d1bfab79af067de75d2b42b
Christoph Diehl <cdiehl@mozilla.com>
2017-04-06 16:18:36 +0200

    Cleanup MersenneTwister

--- random/mersennetwister.js
+++ random/mersennetwister.js
@@ -81,6 +81,6 @@ function MersenneTwister()
   };
 
   this.genrand_real2 = function () {
-    return this.genrand_int32() * (1.0 / 4294967296.0);
+    return this.int32() * (1.0 / 4294967296.0);
   }
 }
