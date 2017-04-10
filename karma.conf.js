a9dd31aed25a69d3809a7de610f4452bdb708f11
Jesse Schwartzentruber <truber@mozilla.com>
2017-04-10 14:03:05 -0400

    Enable Firefox in Karma config for Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
     };
 
     if (process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_travis_ci'];
+        configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
     }
 
     config.set(configuration);
