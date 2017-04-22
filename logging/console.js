77931baa0b97f26f5c7fc8c2d45d07e7c21453b2
Christoph Diehl <cdiehl@mozilla.com>
2017-04-23 00:27:00 +0300

    [console.js] Lowercase object name

--- logging/console.js
+++ logging/console.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var websocket = null;
+
 var logger = (function () {
   let color = {
     red: "\033[1;31m",
