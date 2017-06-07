1f061e996fb5404f1fc8335f15e53ddb5d5a34aa
Jesse Schwartzentruber <truber@mozilla.com>
2017-06-07 11:42:09 -0400

    Use Travis CI to automate build and deploy. Latest octo.js will always be on github pages.

--- build.py
+++ build.py
@@ -1,25 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
 from __future__ import print_function
 
 import argparse
 import os
-import sys
-from shutil import copy
 
 # List of files which must be loaded first
-preload = [
+PRELOAD = (
     'random/random.js',
     'random/mersennetwister.js',
     'utils/init.js',
     'utils/platform.js',
     'logging/console.js',
     'make/init.js'
-]
+)
 
 
 def argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--libs', required=True, help='Directory containing the octo libraries.')
-    parser.add_argument('-d', '--deploy', required=True, help='Directory where the combined octo library will be built.')
+    parser.add_argument('-l', '--libs', required=True,
+                        help='Directory containing the octo libraries.')
+    parser.add_argument('-d', '--deploy', required=True,
+                        help='Directory where the combined octo library will be built.')
     args = parser.parse_args()
 
     if not os.path.isdir(args.libs):
@@ -34,28 +36,28 @@ def main():
     args = argparser()
 
     data = []
-    loaded = []
+    loaded = set()
 
-    for filename in preload:
+    for filename in PRELOAD:
         path = os.path.join(args.libs, filename)
         if not os.path.isfile(path):
             raise Exception("Unable to find file in preload: {0}".format(path))
         print("Adding path: {0}".format(path))
-        with open(path) as f:
+        with open(path, 'rb') as f:
             data.append(f.read())
-            loaded.append(path)
+            loaded.add(path)
 
-    for root, dirs, files in os.walk(args.libs):
+    for root, _, files in os.walk(args.libs):
         for filename in files:
             path = os.path.join(root, filename)
             if path.endswith('.js') and path not in loaded:
                 print("Adding path: {0}".format(path))
-                with open(path) as f:
+                with open(path, 'rb') as f:
                     data.append(f.read())
-    
+
     octo_path = os.path.join(args.deploy, 'octo.js')
-    with open(octo_path, 'w') as f:
-        f.write('\n\n'.join(data))
+    with open(octo_path, 'wb') as f:
+        f.write(b'\n\n'.join(data))
 
 
 if __name__ == "__main__":
