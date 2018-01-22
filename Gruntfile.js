94938bda2878bd8ab3a382168a6ac78eb8c53bcc
Christoph Diehl <1614333+posidron@users.noreply.github.com>
2018-01-22 22:30:40 +0100

    Add pre-commit hook for standard linter

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   let pkg = grunt.file.readJSON('package.json')
 
   grunt.initConfig({
--- karma.conf.js
+++ karma.conf.js
@@ -1,60 +1,60 @@
-module.exports = function(config) {
-    var configuration = {
-        basePath: './tests',
+module.exports = function (config) {
+  let configuration = {
+    basePath: './tests',
 
-        frameworks: ['qunit'],
+    frameworks: ['qunit'],
 
-        files: [
-          '../lib/utils/init.js',
-          '../lib/utils/*.js',
-          '../lib/logging/*.js',
-          '../lib/make/init.js',
-          '../lib/make/*.js',
-          '../lib/random/*.js',
-          '**/*.js'
-        ],
+    files: [
+      '../lib/utils/init.js',
+      '../lib/utils/*.js',
+      '../lib/logging/*.js',
+      '../lib/make/init.js',
+      '../lib/make/*.js',
+      '../lib/random/*.js',
+      '**/*.js'
+    ],
 
-        exclude: [
-        ],
+    exclude: [
+    ],
 
-        preprocessors: {
-          '../lib/**/*.js': ['coverage']
-        },
+    preprocessors: {
+      '../lib/**/*.js': ['coverage']
+    },
 
-        reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage'],
 
-        port: 9876,
+    port: 9876,
 
-        colors: true,
+    colors: true,
 
-        logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO,
 
-        autoWatch: true,
+    autoWatch: true,
 
-        browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox'],
 
-        singleRun: true,
+    singleRun: true,
 
-        browserNoActivityTimeout: 30000,
+    browserNoActivityTimeout: 30000,
 
-        customLaunchers: {
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
-        },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
-        coverageReporter: {
-          reporters: [
-            { type: "lcov", dir: "coverage/" },
-            { type: 'text-summary' }
-          ]
-        },
-    };
-
-    if (process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', dir: 'coverage/' },
+        { type: 'text-summary' }
+      ]
     }
+  }
 
-    config.set(configuration);
-};
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci', 'Firefox']
+  }
+
+  config.set(configuration)
+}
