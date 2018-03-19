360f7abb1ea584b6400bf705be6e94dee0ddda57
Christoph Diehl <1614333+posidron@users.noreply.github.com>
2018-03-19 04:54:15 +0100

    Add entry point for node package

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const logger = require('./lib/logging')
-const make = require('./lib/make')
-const random = require('./lib/random')
-const utils = require('./lib/utils')
-
-module.exports = {
-    logger,
-    random,
-    make,
-    utils
-}
+module.exports = require('./lib/')
