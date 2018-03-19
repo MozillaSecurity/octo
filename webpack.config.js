43b69880b1212599e531599d28eac9dc5432cd17
Christoph Diehl <1614333+posidron@users.noreply.github.com>
2018-03-19 05:54:40 +0100

    Add linter fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path')
-const webpack = require("webpack")
 
 let config = {
-    entry: './lib/index.js',
-    output: {
-        filename: 'octo.js',
-        library: 'octo',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'var'
-    }
+  entry: './lib/index.js',
+  output: {
+    filename: 'octo.js',
+    library: 'octo',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'var'
+  }
 }
 
 module.exports = config
