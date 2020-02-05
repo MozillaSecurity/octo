8413f6822fbaa059e25921266a742bfe54f84049
pyoor <pyoor@users.noreply.github.com>
2020-02-05 09:55:43 -0500

    Add RTC userFragment generator

--- lib/make/network.ts
+++ lib/make/network.ts
@@ -6,6 +6,10 @@ import { random } from '../random'
 import { utils } from '../utils'
 
 export class network {
+  static ufrag () {
+    return 'f5fda439'
+  }
+
   static sdp () {
     // session description protocol template
     return [
@@ -13,7 +17,7 @@ export class network {
       'o=Mozilla-SIPUA 23597 0 IN IP4 0.0.0.0',
       's=SIP Call',
       't=0 0',
-      'a=ice-ufrag:f5fda439',
+      `a=ice-ufrag:${this.ufrag()}`,
       'a=ice-pwd:d0df8e2904bdbd29587966e797655970',
       'a=fingerprint:sha-256 DF:69:78:20:8D:2E:08:CE:49:82:A3:11:79:1D:BF:B5:49:49:2D:32:82:2F:0D:88:84:A7:C6:63:23:63:A9:0F',
       'm=audio 52757 RTP/SAVPF 109 0 8 101',
