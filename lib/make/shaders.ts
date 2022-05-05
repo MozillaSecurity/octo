f1a98a6e3d0d03686a68dfde19827c4e78cf8ea8
pyoor <pyoor@users.noreply.github.com>
2022-05-05 10:08:03 -0400

    refactor: remove unused imports and variables

--- lib/make/shaders.ts
+++ lib/make/shaders.ts
@@ -1,65 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-import { random } from "../random"
-import { utils } from "../utils"
-
-const webgl2VertexShader = `#version 300 es
-precision highp float;
-uniform bool u_bool;
-uniform bvec2 u_bvec2;
-uniform bvec3 u_bvec3;
-uniform bvec4 u_bvec4;
-uniform float u_float;
-uniform int u_int;
-uniform ivec2 u_ivec2;
-uniform ivec3 u_ivec3;
-uniform ivec4 u_ivec4;
-uniform mat2 u_mat2;
-uniform mat3 u_mat3;
-uniform mat4 u_mat4;
-uniform mat2x3 u_mat2x3;
-uniform mat2x4 u_mat2x4;
-uniform mat3x2 u_mat3x2;
-uniform mat3x4 u_mat3x4;
-uniform mat4x2 u_mat4x2;
-uniform mat4x3 u_mat4x3;
-uniform uint u_uint;
-uniform uvec2 u_uvec2;
-uniform uvec3 u_uvec3;
-uniform uvec4 u_uvec4;
-uniform vec2 u_vec2;
-uniform vec3 u_vec3;
-uniform vec4 u_vec4;
-in vec3 position;
-out vec2 v_vec2;
-out vec3 v_vec3;
-void main() {
-  gl_Position.x += u_float;
-  gl_Position.x += u_vec2.x;
-  gl_Position.x += u_vec3.x;
-  gl_Position.x += u_vec4.x;
-  gl_Position.x += u_mat2[0][0];
-  gl_Position.x += u_mat3[0][0];
-  gl_Position.x += u_mat4[0][0];
-  gl_Position.x += u_mat2x3[0][0];
-  gl_Position.x += u_mat2x4[0][0];
-  gl_Position.x += u_mat3x2[0][0];
-  gl_Position.x += u_mat3x4[0][0];
-  gl_Position.x += u_mat4x2[0][0];
-  gl_Position.x += u_mat4x3[0][0];
-  gl_Position.x += float(u_int);
-  gl_Position.x += float(u_uint);
-  gl_Position.x += float(u_ivec2.x);
-  gl_Position.x += float(u_ivec3.x);
-  gl_Position.x += float(u_ivec4.x);
-  gl_Position.x += float(u_uvec2.x);
-  gl_Position.x += float(u_uvec3.x);
-  gl_Position.x += float(u_uvec4.x);
-  if (u_bool || u_bvec2.x || u_bvec3.x || u_bvec4.x) {
-    gl_Position += float(0.1);
-  }
-}`
 
 /**
  * Class for generating WebGL shader related values.
