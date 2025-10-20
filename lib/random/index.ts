7c414ff39985007401c70489524425e4e2cffb53
pyoor <pyoor@users.noreply.github.com>
2025-10-20 14:49:51 -0400

    fix: explicitly mark return type as readonly

--- lib/random/index.ts
+++ lib/random/index.ts
@@ -192,7 +192,7 @@ export class Random {
    * Return a shuffled copy of an array.
    * @param arr - Source Array to shuffle.
    */
-  shuffled<T>(arr: readonly T[]): T[] {
+  shuffled<T>(arr: readonly T[]): readonly T[] {
     const newArray = arr.slice()
     this.shuffle(newArray)
     return newArray
